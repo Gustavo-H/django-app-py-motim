685be98dfec94dd576e698cee7fa045b8b92ad56
Gustavo-H <Gustavohps58@gmail.com>
2024-06-25 14:58:07 -0300

    Finalizando projeto

--- loja/urls.py
+++ loja/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    #path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    #TODO Remover Toolbarr
-    path("__debug__/", include("debug_toolbar.urls")),
-]
+
